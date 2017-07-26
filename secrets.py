3dac279f1de2c49759d2c8e80a08cabaa1a1588f
testbotthing <30478927+testbotthing@users.noreply.github.com>
2017-07-26 15:54:41 -0500

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,4 @@
+-consumer_key = "318gg061MCinlUZtoi0754qs8"
+-consumer_secret = "1b0ZgDiOyKnMONTDV9lMEBO4BA4NKm91VwG8RGXY7VjpuiVq2E"
+-access_token = "889942691701825536-JYHFnVxMG77nDEVUibMnxeXH5pnE6PD"
+-access_token_secret = "ih4eoRlLEg7TRH8d4QtdEui8fUCCsmS5r4FyzPtuLZy5h"
